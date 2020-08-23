3f39862d347fc86b6aad7d09c72d36a974752fde
MikeC <kiragumike@gmail.com>
2020-08-23 19:12:30 +0300

    exported quotes

--- src/app/quotes-details/quotes-details.component.ts
+++ src/app/quotes-details/quotes-details.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {Quotes} from '../quotes'
 @Component({
   selector: 'app-quotes-details',
   templateUrl: './quotes-details.component.html',
   styleUrls: ['./quotes-details.component.css']
 })
 export class QuotesDetailsComponent implements OnInit {
+  @Input() quotes: Quotes
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  // quotesComplete(complete:boolean){
+  //   this.isComplete.emit(complete)
+  // }
+  quoteDelete(complete:boolean){
+    this.isComplete.emit(complete)
+  }
 
   constructor() { }
 
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,7 +7,38 @@ import { Quotes } from '../quotes';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-@Input() quotes: Quotes
+ quotes: Quotes[]=[
+  new Quotes(1,'Spread love everywhere you go. Let no one ever come to you without leaving happier', 'Mother Teresa', new Date(1990,3,14)),
+  new Quotes (2,'When you reach the end of your rope, tie a knot in it and hang on','Franklin D. Roosevelt', new Date(1993,2,15)),
+  new Quotes (3, 'We cannot solve our problems with the same thinking we used when we created them','Albert Einstein', new Date(1900,1,20)),
+  new Quotes (4, 'If you can dream it, you can do it','Walt Disney', new Date(2000,5,10)),
+ ]
+ toggleDetails(index: string| number){
+   this.quotes[index].showDescription = !this.quotes[index].showDescription;
+ }
+ completeQuotes(isComplete: any, index: number){
+   if (isComplete){
+     this.quotes.splice(index,1)
+    }
+ }
+ deleteQuotes(isComplete: any,index: number){
+   if (isComplete){
+    let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+
+    if (toDelete){
+      this.quotes.splice(index,1)
+    }
+   }
+ }
+ addNewQuote(quotes: Quotes){
+  let QuotesLength = this.quotes.length;
+  quotes.id = QuotesLength+1;
+  quotes.completeDate = new Date(quotes.completeDate)
+  this.quotes.push(quotes)
+}
+
+
+
   constructor() { }
 
   ngOnInit(): void {
