557afc465b335ea7075e665ef390788d13b345fa
MikeC <kiragumike@gmail.com>
2020-08-24 01:26:20 +0300

    added directives

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-
 import {Quotes} from '../quotes'
+
 @Component({
   selector: 'app-quotes-form',
   templateUrl: './quotes-form.component.html',
